663fd98310e2796a22f6279e39189439b3e192ee
AidenFrank <aidensafrank@gmail.com>
2025-02-17 08:34:01 -0500

    Implemented Subkey Schedule

--- aes_14361165/src/aes_algorithm.py
+++ aes_14361165/src/aes_algorithm.py
@@ -48,6 +48,14 @@ def PrintBlock(block):
         for row in range (0, 4):
             print(format(block[row][col], "02x"), end=" ")
         print(" ")
+
+# This just prints a block as a hex
+def PrintHex(block):
+    result = "0x"
+    for row in range(0, 4):
+        for col in range(0, 4):
+            result += format(block[row][col], "02x")
+    print(result)
             
 # This performs an AddKey opertion on two blocks of 4 x 4 lists
 def AddKey(block1, block2):
@@ -76,9 +84,10 @@ def ShiftRows(block1):
     # Create a block to hold the computed data
     block2 = [[0]* 4 for j in range(4)]
     # We shift each row left by n, the number of the row (first row shifts zero, second shifts 1, etc.)
-    # TODO: Make this an actual algorithm
+    # The first row isn't shifted at all, so we just set it equal to our original values
     for x in range (0, 4):
         block2[x][0] = block1[x][0]
+    # Now we just set each block value to the corresponding value it should be after shift
     block2[0][1] = block1[1][1]
     block2[1][1] = block1[2][1]
     block2[2][1] = block1[3][1]
@@ -121,6 +130,15 @@ def MixColumns(block1):
             block2[mulRow].append(result)
     return block2
 
+def LeftCircularShift(word):
+    # We don't need any bitwise operations because our words are just lists of 4 bytes, so we just reassign the bytes
+    tmp = []
+    tmp.append(word[1])
+    tmp.append(word[2])
+    tmp.append(word[3])
+    tmp.append(word[0])
+    return(tmp)
+
 # This goes through all of the steps of a round of AES
 def Round(block, subkeyblock):
     subBytesBlock = SubBytes(block)
@@ -129,13 +147,54 @@ def Round(block, subkeyblock):
     addKeyBlock = AddKey(mixColumnsBlock, subkeyblock)
     return addKeyBlock
 
-# This just prints a block as a hex
-def PrintHex(block):
-    result = "0x"
+def SubkeySchedule(previousSubkeyBlock, roundConstant):
+    # We create a block to hold our words
+    w = [[0]* 4 for j in range(4)]
     for row in range(0, 4):
         for col in range(0, 4):
-            result += format(block[row][col], "02x")
-    print(result)
+            w[row][col] = previousSubkeyBlock[row][col]
+    # We keep track of the original last word of the block and its hex value
+    w3Original = []
+    for i in range(0, 4):
+        w3Original.append(w[3][i])
+    w3OriginalHex = "0x"
+    for i in range(0, 4):
+        w3OriginalHex += format(w[3][i], "02x")
+    # We perform g function on the last word in our block: Left Circular Shift, SubBytes, XOR with round constant
+    w[3] = LeftCircularShift(w[3])
+    w[3] = SubBytes([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], w[3]])[3]
+    w3Hex = "0x"
+    for i in range(0, 4):
+        w3Hex += format(w[3][i], "02x")
+    w3Hex = int(w3Hex, 16) ^ roundConstant
+    # We put the new last word back into the block
+    for i in range(0, 4):
+        w[3][i] = int(format(w3Hex, "02x")[i*2:(i*2+2)], 16)
+    # We start formatting every word has a string of hex so that it can be XORed
+    hexWords = []
+    # Create y block to be used as the output of the function
+    y = [[0]* 4 for j in range(4)]
+    for i in range(0, 4):
+        tmp = "0x"
+        for j in range(0, 4):
+            tmp += format(w[i][j], "02x")
+        hexWords.append(tmp)
+    # Now we use tmpWord to get the output of the XOR and then extract the hex values from it, convert to int
+    # and put it into our y block
+    tmpWord = format(int(hexWords[0], 16) ^ int(hexWords[3], 16), "02x")
+    for i in range(0, 4):
+        y[0][i] = int(tmpWord[i*2:(i*2+2)], 16)
+    tmpWord = format(int(tmpWord, 16) ^ int(hexWords[1], 16), "02x")
+    for i in range(0, 4):
+        y[1][i] = int(tmpWord[i*2:(i*2+2)], 16)
+    tmpWord = format(int(tmpWord, 16) ^ int(hexWords[2], 16), "02x")
+    for i in range(0, 4):
+        y[2][i] = int(tmpWord[i*2:(i*2+2)], 16)
+    # Note that we XOR with the original last word of the w block
+    tmpWord = format(int(tmpWord, 16) ^ int(w3OriginalHex, 16), "02x")
+    for i in range(0, 4):
+        y[3][i] = int(tmpWord[i*2:(i*2+2)], 16)
+    return(y)
 
 def encryption(plaintext, subkeyFile):
     # Take the plaintext and convert it all into hex, then put in a list
@@ -168,8 +227,12 @@ def encryption(plaintext, subkeyFile):
     # We now have arrays of 4x4 blocks for the plaintext and subkeys
     # We perform the initial transformation, AddKey with subkey 0
     initialTrans = AddKey(plaintextBlocks[0], subkeyBlocks[0])
+    # We use our initial subkey to get Subkey 1
+    subkey1 = SubkeySchedule(subkeyBlocks[0], 0x01000000)
+    print("Subkey 1:")
+    PrintHex(subkey1)
     # We perform the first round with our initial transformation and subkey 1
-    round1 = Round(initialTrans, subkeyBlocks[1])
+    round1 = Round(initialTrans, subkey1)
     # We print the first round output
     print("Round 1 output:")
     PrintHex(round1)
